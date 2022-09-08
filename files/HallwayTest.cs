5549946dfbe8edd13d94c97d990b1b5de1119ab1
Dk2rKF <Dk2rKF@cyber-dojo.org>
2022-09-08 20:21:21 +0000

    2 ran tests, no prediction, got amber

--- files/HallwayTest.cs
+++ files/HallwayTest.cs
@@ -1,3 +1,30 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 [Test]
 public void ToggleDoorOne()
 {
@@ -6,5 +33,25 @@ public void ToggleDoorOne()
 }
 
 [Test]
-public void 
+public void DoorNumbmer2Status()
+{
+    hallway.Toggle(1);
+    Assert.AreEqual(true, hallway.IsDoorOpen(1));
+    Assert.AreEqual(false, hallway.IsDoorOpen(0));
+}
+
+[Test]
+public void ToggleTwice()
+{
+    hallway.Toggle(0);
+    hallway.Toggle(0);
+    Assert.AreEqual(false, hallway.IsDoorOpen(0));
+}
+[Test]
+public void ToArray()
+{
+    CollectionAssert.AreEqual(Enumerable.Repeat(false, 100).ToArray(), hallway.ToArray());
+}
+
+
     
